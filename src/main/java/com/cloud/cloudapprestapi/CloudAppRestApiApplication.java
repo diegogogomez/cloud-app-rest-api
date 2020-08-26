57eede3ca67920d8227030dcf6ae2b514880ba80
Diego Gomez <d.fernandogomez94@gmail.com>
2020-08-26 18:42:27 -0500

    Primer commit

---
+++ src/main/java/com/cloud/cloudapprestapi/CloudAppRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.cloudapprestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudAppRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudAppRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloud/cloudapprestapi/CloudAppRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.cloudapprestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudAppRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
