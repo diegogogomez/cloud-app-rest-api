5b5e089ce3d03d4498df6925cf5f3cbab1e3ebb2
Diego Gomez <d.fernandogomez94@gmail.com>
2020-08-26 21:58:29 -0500

    tercer commit

--- src/main/java/com/cloud/cloudapprestapi/rest/FunRestController.java
+++ src/main/java/com/cloud/cloudapprestapi/rest/FunRestController.java
@@ -1,4 +1,17 @@
 package com.cloud.cloudapprestapi.rest;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
 public class FunRestController {
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World! Time on server is " + LocalDateTime.now() + "<h1><font face='verdana'>Spring Boot Rocks!</font></h1>";
+    }
+
 }
