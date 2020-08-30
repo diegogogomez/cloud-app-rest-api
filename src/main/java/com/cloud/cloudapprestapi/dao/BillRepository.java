09f29652a4c190442d9eb3d35540c2ed3439e5a7
Diego Gomez <d.fernandogomez94@gmail.com>
2020-08-30 11:27:55 -0500

    Commit Agregar clases entidades y endpoints cross3

--- src/main/java/com/cloud/cloudapprestapi/dao/BillRepository.java
+++ src/main/java/com/cloud/cloudapprestapi/dao/BillRepository.java
@@ -2,7 +2,10 @@ package com.cloud.cloudapprestapi.dao;
 
 import com.cloud.cloudapprestapi.web.model.BillDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+
+@CrossOrigin
 public interface BillRepository extends JpaRepository<BillDto, Integer>, BillRepositoryCustom {
 
 }
--- src/main/java/com/cloud/cloudapprestapi/dao/ProductRepository.java
+++ src/main/java/com/cloud/cloudapprestapi/dao/ProductRepository.java
@@ -3,7 +3,9 @@ package com.cloud.cloudapprestapi.dao;
 import com.cloud.cloudapprestapi.web.model.ProductDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(collectionResourceRel = "product", path = "product")
+@CrossOrigin
 public interface ProductRepository extends JpaRepository<ProductDto, Integer>, ProductRepositoryCustom {
 }
